736ebe8952b6fe7bd432ac70eb505ae69f635d73
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-19 12:00:19 +0100

    feat: public navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { Header } from "./components/layout/general/Header"
+// import { Header } from "./components/layout/general/Header"
+import { Header } from "./components/layout/public/Header"
 
 function App() {
 
   return (
     <div className="layout">
       <Header />
+
+      <section className="layout__content">
+
+      </section>
     </div>
   )
 }
