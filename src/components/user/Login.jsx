50eb5153bd67675d2dfae7b3d37c5e6f3602b96b
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-20 13:34:34 +0100

    feat: public navigation

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Login = () => {
     return (
-        <div>Login</div>
+        <>
+            <header className='content__header'>
+                <h1 className='content__title'>Login</h1>
+            </header>
+
+            <div className="content__posts">
+
+            </div>
+        </>
     )
 }
--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Register = () => {
     return (
-        <div>Register</div>
+        <>
+            <header className='content__header'>
+                <h1 className='content__title'>Register</h1>
+            </header>
+
+            <div className="content__posts">
+
+            </div>
+        </>
     )
 }
