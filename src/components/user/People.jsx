fc720695cde6512a88857af27a9b402d01a90a46
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-27 13:56:38 +0100

    feat: get users list

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from '../../assets/img/user.png'
-
+import { Global } from '../../helpers/Global'
 export const People = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const request = await fetch(Global.url + "/users/list/1", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token"),
+            }
+        });
+
+        const data = await request.json();
+
+        if (data.users && data.status === "success") {
+            setUsers(data.users);
+        }
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+
+
     return (
         <>
             <header className="content__header">
@@ -9,36 +34,44 @@ export const People = () => {
             </header>
 
             <div className="content__posts">
-                <article className="posts__post">
-                    <div className="post__container">
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
+                {
+                    users.map(user => {
+                        return (
+                            <article key={user.id} className="posts__post">
+                                <div className="post__container">
+                                    <div className="post__image-user">
+                                        <a href="#" className="post__image-link">
+                                            {user.image !== "default.png" && <img src={Global.url + "/users/image/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                                            {user.image === "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
+                                        </a>
+                                    </div>
 
-                        <div className="post__body">
+                                    <div className="post__body">
 
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
-                            </div>
+                                        <div className="post__user-info">
+                                            <a href="#" className="user-info__name">{user.name} {user.lastname}</a>
+                                            <span className="user-info__divider"> | </span>
+                                            <a href="#" className="user-info__create-date">{user.created_at}</a>
+                                        </div>
 
-                            <h4 className="post__content">Hola, buenos dias.</h4>
-                        </div>
-                    </div>
+                                        <h4 className="post__content">{user.bio}</h4>
+                                    </div>
+                                </div>
 
-                    <div className="post__buttons">
-                        <a href="#" className="post__button post__button--green">
-                            Follow
-                        </a>
+                                <div className="post__buttons">
+                                    <a href="#" className="post__button post__button--green">
+                                        Follow
+                                    </a>
 
-                        {/* <a href="#" className="post__button">
-                            Unfollow
-                        </a> */}
-                    </div>
-                </article>
+                                    {/* <a href="#" className="post__button">
+                                        Unfollow
+                                    </a> */}
+                                </div>
+                            </article>
+
+                        )
+                    })
+                }
             </div>
 
             <div className="content__container-btn">
