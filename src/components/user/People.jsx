dd9b29e9fa17d35300e81f33d61cb40a88350728
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-27 13:43:06 +0100

    feat: people component structure

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import avatar from '../../assets/img/user.png'
 
 export const People = () => {
     return (
-        <div>People</div>
+        <>
+            <header className="content__header">
+                <h1 className="content__title">People</h1>
+            </header>
+
+            <div className="content__posts">
+                <article className="posts__post">
+                    <div className="post__container">
+                        <div className="post__image-user">
+                            <a href="#" className="post__image-link">
+                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
+                            </a>
+                        </div>
+
+                        <div className="post__body">
+
+                            <div className="post__user-info">
+                                <a href="#" className="user-info__name">Victor Robles</a>
+                                <span className="user-info__divider"> | </span>
+                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                            </div>
+
+                            <h4 className="post__content">Hola, buenos dias.</h4>
+                        </div>
+                    </div>
+
+                    <div className="post__buttons">
+                        <a href="#" className="post__button post__button--green">
+                            Follow
+                        </a>
+
+                        {/* <a href="#" className="post__button">
+                            Unfollow
+                        </a> */}
+                    </div>
+                </article>
+            </div>
+
+            <div className="content__container-btn">
+                <button className="content__btn-more-post">
+                    More people
+                </button>
+            </div>
+        </>
     )
 }
